3aa7f4170fc03ccd851a28428fda54705a5909a1
Jagu4491 <jagrutisheladiya4491@gmail.com>
2023-01-08 11:45:28 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class myhomepage extends StatelessWidget{
   Widget build(BuildContext context) {
   return Scaffold(
     appBar: AppBar(
-      title: Text('Myapp'),
+      title: Text('myapp'),
     ),
   );
   }

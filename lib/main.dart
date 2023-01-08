e0461b8c1be9abe6672ef5a6644f0a6a761c3816
Jagu4491 <jagrutisheladiya4491@gmail.com>
2023-01-08 10:03:40 +0530

    Intiall Commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myapp());
+}
+class myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'my app',
+      theme: ThemeData(
+        primarySwatch: Colors.green
+      ),
+      home: myhomepage(),
+    );
+  }
+
+}
+
+class myhomepage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Myapp'),
+    ),
+  );
+  }
+
+}
